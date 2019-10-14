2ab14248855aedaa620556345ba6ec18859568b8
Jeff Lai <winfield301@gmail.com>
2019-10-14 17:41:59 +0800

    Config up server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
+extern crate actix_web;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("8080"));
+
+    HttpServer::new(move || App::new().route("/", web::get().to(index)))
+        .bind(format!("127.0.0.1:{}", port))
+        .unwrap()
+        .run()
+        .unwrap();
 }
