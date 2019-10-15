7352f1973f8f2f68c2ab6ed89e3061191e98e433
Jeff Lai <winfield301@gmail.com>
2019-10-15 17:39:56 +0800

    Set up basic wasm

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen(start)]
+pub fn greet() {
+    alert("Hello wasm!")
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
