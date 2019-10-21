d3065f7b7268f886de5f981859caa613d89245e9
Jeff Lai <winfield301@gmail.com>
2019-10-21 12:41:53 +0800

    Add basic askama templates

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,27 @@
 extern crate actix_web;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use askama::Template;
+use std::collections::HashMap;
 use std::env;
+
+#[derive(Template)]
+#[template(path = "authorize.html")]
+struct AuthorizeTemplate<'a> {
+    name: &'a str,
+    text: &'a str,
+}
+
+fn authorize(query: web::Query<HashMap<String, String>>) -> impl Responder {
+    let s = AuthorizeTemplate {
+        name: "Guys",
+        text: "Welcome!",
+    }
+    .render()
+    .unwrap();
+
+    HttpResponse::Ok().content_type("text/html").body(s)
+}
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,9 +30,13 @@ fn index() -> impl Responder {
 fn main() {
     let port = env::var("PORT").unwrap_or_else(|_| String::from("8080"));
 
-    HttpServer::new(move || App::new().route("/authorize", web::get().to(index)))
-        .bind(format!("127.0.0.1:{}", port))
-        .unwrap()
-        .run()
-        .unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/authorize", web::get().to(authorize))
+    })
+    .bind(format!("0.0.0.0:{}", port))
+    .unwrap()
+    .run()
+    .unwrap();
 }
