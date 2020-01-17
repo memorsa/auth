f9db397677a8ab28560861eda34e49bc9a2861f5
Jeff Lai <winfield301@gmail.com>
2020-01-17 22:06:14 +0800

    Put db connection logic into separate module

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,56 +1,15 @@
-use std::io;
-use std::sync::Arc;
+use tokio_postgres::{connect as db_connect, Client, Error, NoTls};
 
-use actix::prelude::*;
-use bytes::{BufMut, Bytes, BytesMut};
-use futures::FutureExt;
-use tokio_postgres::{connect, Client, NoTls};
+pub async fn connect(db_url: &str) -> Result<Client, Error> {
+    let (cl, conn) = db_connect(db_url, NoTls).await?;
 
-/// Postgres interface
-pub struct PgConnection {
-    cl: Arc<Client>,
-}
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
 
-pub struct RandomWorld;
-
-impl Message for RandomWorld {
-    type Result = io::Result<Bytes>;
-}
-
-impl Handler<RandomWorld> for PgConnection {
-    type Result = ResponseFuture<Result<Bytes, io::Error>>;
-
-    fn handle(&mut self, _: RandomWorld, _: &mut Self::Context) -> Self::Result {
-        let cl = self.cl.clone();
-        Box::pin(async move {
-            let fut = cl.query("SELECT $1::TEXT", &[&"hello world Actor"]);
-            let rows = fut
-                .await
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
-            let value: &str = rows[0].get(0);
-
-            let mut body = BytesMut::with_capacity(40);
-
-            body.put(value.clone().as_bytes());
-
-            Ok(body.freeze())
-        })
-    }
-}
-
-impl Actor for PgConnection {
-    type Context = Context<Self>;
-}
-
-impl PgConnection {
-    pub async fn connect(db_url: &str) -> Result<Addr<PgConnection>, io::Error> {
-        let (cl, conn) = connect(db_url, NoTls)
-            .await
-            .expect("can not connect to postgresql");
-        actix_rt::spawn(conn.map(|_| ()));
-
-        Ok(PgConnection::create(move |_| PgConnection {
-            cl: Arc::new(cl),
-        }))
-    }
+    Ok(cl)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 use tokio_postgres::NoTls;
 use warp::Filter;
 
+mod db;
+
+use db::connect;
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
@@ -13,17 +17,8 @@ async fn main() {
 
     const DB_URL: &str = "postgres://ryzfreof:qiwHjU94MS5jOkkDVoChIg-m5-3_8NZO@rajje.db.elephantsql.com:5432/ryzfreof";
 
-    let (client, connection) = tokio_postgres::connect(DB_URL, NoTls).await.unwrap();
+    let client = connect(DB_URL).await.unwrap();
 
-    // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    // Now we can execute a simple statement that just returns its parameter.
     let rows = client
         .query("SELECT $1::TEXT", &[&"hello world!"])
         .await
