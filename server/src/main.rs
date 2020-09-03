cbf2c2ba9b2ece6f6b0461c0ca7baf19ad1e8ad4
Jeff Lai <winfield301@gmail.com>
2020-09-02 23:33:08 -0700

    Implement register & login

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,9 @@
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use warp::http::StatusCode;
 use warp::Filter;
 
 mod db;
@@ -8,6 +11,12 @@ use db::connect;
 
 mod access_token;
 mod authorize;
+
+#[derive(Deserialize)]
+struct User {
+    username: String,
+    password: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -32,8 +41,20 @@ async fn main() {
     // let static_files = warp::path("static").and(warp::fs::dir("./client/public"));
     // let routes = static_files.or(authorize::routes()).or(access_token::routes());
 
-    let register = warp::path("register").map(|| "Hello from register");
-    let login = warp::path("login").map(|| "Hello from login");
+    let db = Arc::new(Mutex::new(HashMap::<String, User>::new()));
+    let db = warp::any().map(move || Arc::clone(&db));
+
+    let register = warp::post()
+        .and(warp::path("register"))
+        .and(warp::body::json())
+        .and(db.clone())
+        .and_then(register);
+    let login = warp::post()
+        .and(warp::path("login"))
+        .and(warp::body::json())
+        .and(db.clone())
+        .and_then(login);
+
     let logout = warp::path("logout").map(|| "Hello from logout");
     let api = warp::path("api").and(register.or(login).or(logout));
 
@@ -55,4 +76,33 @@ async fn counter(db: Arc<Mutex<u8>>) -> Result<impl warp::Reply, warp::Rejection
     let mut counter = db.lock().await;
     *counter += 1;
     Ok(counter.to_string())
+}
+
+async fn register(
+    new_user: User,
+    db: Arc<Mutex<HashMap<String, User>>>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut users = db.lock().await;
+    if users.contains_key(&new_user.username) {
+        return Ok(StatusCode::BAD_REQUEST);
+    }
+    users.insert(new_user.username.clone(), new_user);
+    Ok(StatusCode::CREATED)
+}
+
+async fn login(
+    credentials: User,
+    db: Arc<Mutex<HashMap<String, User>>>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let users = db.lock().await;
+    match users.get(&credentials.username) {
+        None => Ok(StatusCode::BAD_REQUEST),
+        Some(user) => {
+            if credentials.password == user.password {
+                Ok(StatusCode::OK)
+            } else {
+                Ok(StatusCode::UNAUTHORIZED)
+            }
+        }
+    }
 }
