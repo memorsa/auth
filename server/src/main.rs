b34c305ccd3a742ea269a7a2eae3d17cc316f18f
Jeff Lai <winfield301@gmail.com>
2020-09-07 23:30:31 -0700

    Cleanup main

--- server/src/main.rs
+++ server/src/main.rs
@@ -36,90 +36,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .connect(DB_URL)
         .await?;
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
+    let static_files = warp::path("static").and(warp::fs::dir("./client/public"));
 
-    assert_eq!(row.0, 150);
-
-    // let client = connect(DB_URL).await.unwrap();
-
-    // let rows = client
-    //     .query("SELECT $1::TEXT", &[&"hello wrap!"])
-    //     .await
-    //     .unwrap();
-
-    // let value: String = rows[0].get(0);
-
-    // let static_files = warp::path("static").and(warp::fs::dir("./client/public"));
-    // let routes = static_files.or(authorize::routes()).or(access_token::routes());
-
-    let db = Arc::new(Mutex::new(HashMap::<String, User>::new()));
-    let db = warp::any().map(move || Arc::clone(&db));
-
-    let register = warp::post()
-        .and(warp::path("register"))
-        .and(warp::body::json())
-        .and(db.clone())
-        .and_then(register);
-    let login = warp::post()
-        .and(warp::path("login"))
-        .and(warp::body::json())
-        .and(db.clone())
-        .and_then(login);
-
-    let logout = warp::path("logout").map(|| "Hello from logout");
-    let api = warp::path("api").and(register.or(login).or(logout));
-
-    let counter_db = Arc::new(Mutex::new(0));
-    let counter_db = warp::any().map(move || Arc::clone(&counter_db));
-    let counter = warp::path("counter")
-        .and(counter_db.clone())
-        .and_then(counter);
-
-    let routes = authorize::routes()
+    let routes = static_files
         .or(signup::routes(pool.clone()))
         .or(signin::routes(pool.clone()))
-        .or(access_token::routes())
-        .or(api)
-        .or(counter);
+        .or(authorize::routes())
+        .or(access_token::routes());
 
     warp::serve(routes).run(([0, 0, 0, 0], port)).await;
     Ok(())
-}
-
-async fn counter(db: Arc<Mutex<u8>>) -> Result<impl warp::Reply, warp::Rejection> {
-    let mut counter = db.lock().await;
-    *counter += 1;
-    Ok(counter.to_string())
-}
-
-async fn register(
-    new_user: User,
-    db: Arc<Mutex<HashMap<String, User>>>,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    let mut users = db.lock().await;
-    if users.contains_key(&new_user.username) {
-        return Ok(StatusCode::BAD_REQUEST);
-    }
-    users.insert(new_user.username.clone(), new_user);
-    Ok(StatusCode::CREATED)
-}
-
-async fn login(
-    credentials: User,
-    db: Arc<Mutex<HashMap<String, User>>>,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    let users = db.lock().await;
-    match users.get(&credentials.username) {
-        None => Ok(StatusCode::BAD_REQUEST),
-        Some(user) => {
-            if credentials.password == user.password {
-                Ok(StatusCode::OK)
-            } else {
-                Ok(StatusCode::UNAUTHORIZED)
-            }
-        }
-    }
 }
