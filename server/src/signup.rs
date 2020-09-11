d2ac3950c7253c7e17cc169f7de29fe38c24b8f6
Jeff Lai <winfield301@gmail.com>
2020-09-11 00:20:18 -0700

    Fix signup get path

--- server/src/signup.rs
+++ server/src/signup.rs
@@ -28,9 +28,12 @@ async fn signup(pool: PgPool, new_user: User) -> Result<impl Reply, Rejection> {
 }
 
 pub fn routes(pool: PgPool) -> BoxedFilter<(impl Reply,)> {
-    let get = warp::get().map(|| SignupTemplate { name: "Askama" });
-    let post = warp::post()
-        .and(warp::path("signup"))
+    let path = warp::path("signup");
+    let get = path
+        .and(warp::get())
+        .map(|| SignupTemplate { name: "Askama" });
+    let post = path
+        .and(warp::post())
         .and(warp::any().map(move || pool.clone()))
         .and(warp::body::json())
         .and_then(signup);
