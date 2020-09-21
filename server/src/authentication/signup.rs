e3860d35c94e89df7124d3265c551ba0b9140b4c
Jeff Lai <winfield301@gmail.com>
2020-09-20 22:28:14 -0700

    Extract session logic

--- server/src/authentication/signup.rs
+++ server/src/authentication/signup.rs
@@ -33,27 +33,11 @@ async fn signup(pool: PgPool, new_user: User) -> Result<impl Reply, Rejection> {
     .await
     .unwrap();
 
-    let secret_key_base = env::var("SECRET_KEY_BASE").unwrap();
-    let mut rng: Pcg64 = Seeder::from(secret_key_base).make_rng();
-    let mut master_key = [0u8; 32];
-    rng.fill_bytes(&mut master_key);
-    let key = Key::derive_from(&master_key);
-
-    let mut jar = CookieJar::new();
-    let new_cookie = Cookie::build("user_id", rec.id.to_string())
-        .path("/")
-        .secure(true)
-        .http_only(true)
-        .finish();
-
-    jar.private(&key).add(new_cookie);
-
-    let cookie_header: Vec<String> = jar.iter().map(|cookie| cookie.to_string()).collect();
     let redirect = warp::redirect(Uri::from_static("/"));
     Ok(warp::reply::with_header(
         redirect,
         header::SET_COOKIE,
-        cookie_header.join("; "),
+        session(rec.id.to_string()),
     ))
 }
 
@@ -67,4 +51,25 @@ pub fn routes(pool: PgPool) -> BoxedFilter<(impl Reply,)> {
         .and_then(signup);
 
     get.or(post).boxed()
+}
+
+fn session(user_id: String) -> String {
+    let mut jar = CookieJar::new();
+
+    let secret_key_base = env::var("SECRET_KEY_BASE").unwrap();
+    let mut rng: Pcg64 = Seeder::from(secret_key_base).make_rng();
+    let mut master_key = [0u8; 32];
+    rng.fill_bytes(&mut master_key);
+    let key = Key::derive_from(&master_key);
+
+    let new_cookie = Cookie::build("user_id", user_id)
+        .path("/")
+        .secure(true)
+        .http_only(true)
+        .finish();
+
+    jar.private(&key).add(new_cookie);
+    let cookie_header: Vec<String> = jar.iter().map(|cookie| cookie.to_string()).collect();
+
+    cookie_header.join("; ")
 }
