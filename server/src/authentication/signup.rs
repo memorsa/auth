22ffbf53eb66f92647cde0e34deac461e6888f77
Jeff Lai <winfield301@gmail.com>
2020-09-21 23:45:39 -0700

    Extract session module

--- server/src/authentication/signup.rs
+++ server/src/authentication/signup.rs
@@ -1,12 +1,8 @@
 use super::password_helper::hash;
+use super::session::session;
 use askama::Template;
-use cookie::{Cookie, CookieJar, Key};
-use rand::prelude::*;
-use rand_pcg::Pcg64;
-use rand_seeder::{Seeder, SipHasher};
 use serde::Deserialize;
 use sqlx::postgres::PgPool;
-use std::env;
 use warp::{
     filters::BoxedFilter,
     http::{header, Uri},
@@ -51,25 +47,4 @@ pub fn routes(pool: PgPool) -> BoxedFilter<(impl Reply,)> {
         .and_then(signup);
 
     get.or(post).boxed()
-}
-
-fn session(user_id: String) -> String {
-    let mut jar = CookieJar::new();
-
-    let secret_key_base = env::var("SECRET_KEY_BASE").unwrap();
-    let mut rng: Pcg64 = Seeder::from(secret_key_base).make_rng();
-    let mut master_key = [0u8; 32];
-    rng.fill_bytes(&mut master_key);
-    let key = Key::derive_from(&master_key);
-
-    let new_cookie = Cookie::build("user_id", user_id)
-        .path("/")
-        .secure(true)
-        .http_only(true)
-        .finish();
-
-    jar.private(&key).add(new_cookie);
-    let cookie_header: Vec<String> = jar.iter().map(|cookie| cookie.to_string()).collect();
-
-    cookie_header.join("; ")
 }
